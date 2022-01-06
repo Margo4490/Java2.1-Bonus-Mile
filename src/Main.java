411d39eccb8d342ffae18d9fa6423ab8ef260d83
Margo4490 <95082876+Margo4490@users.noreply.github.com>
2022-01-06 23:49:44 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+        public static void main (String[] args) {
+            // вход
+            int ticketPrice = 3000;
+            int moneyForBonusMile = 20 * 1;
+
+            //расчет
+            int bonusMile = ticketPrice / moneyForBonusMile ;
+
+            System.out.println("Количество бонусных милей за покупку билета - " );
+            System.out.println((bonusMile));
+
+            int receivedBonus = 150;
+        }
+
+
+
+}
